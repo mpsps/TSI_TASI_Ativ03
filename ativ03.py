03b377ec14e6855e40f654cb817bf032a05f023d
mpsps <55263599+mpsps@users.noreply.github.com>
2024-12-30 18:12:14 -0300

    ultimo update
    final

--- ativ03.py
+++ ativ03.py
@@ -1,13 +1,13 @@
 from langchain_community.embeddings import OllamaEmbeddings
 
 # criação de embeddings
-embeddingsText = "Estoicismo é uma escola e doutrina filosófica surgida na Grécia Antiga, que preza a fidelidade ao nconhecimento e o foco em tudo aquilo que pode ser controlado pela própria pessoa. Despreza todos os tipos de sentimentos externos, como a paixão e os desejos extremos."
+documents = "Estoicismo é uma escola e doutrina filosófica surgida na Grécia Antiga, que preza a fidelidade ao nconhecimento e o foco em tudo aquilo que pode ser controlado pela própria pessoa. Despreza todos os tipos de sentimentos externos, como a paixão e os desejos extremos."
 
 # modelo de embeddings do Ollama
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
 # geração do embeddings
-texto_embeddings = embeddings.embed_query(embeddingsText)
+texto_embeddings = embeddings.embed_query(documents)
 
 # tamanho do embeddings
 tamanho_vetor = len(texto_embeddings)
@@ -17,5 +17,5 @@ print(f"------------ Tamanho dos embeddings: {tamanho_vetor} ------------ ")
 print("------------ Os embeddings gerados: -----------------")
 for i in texto_embeddings[:10]:
     print(i)
-    
+
 print("-----------------------------------------------------")
